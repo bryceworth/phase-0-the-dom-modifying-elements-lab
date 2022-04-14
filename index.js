bb459cb56f0a4c734a3dfb854f9dc1c949271931
bryceworth <bryceaworth@gmail.com>
2022-04-13 21:36:31 -0700

    commit tree 7b0526191ca960d0894da2ea92c4e30e85156f5b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+main.remove(main)
+
+const newHeader = document.createElement(`h1`)
+//document.body.appendChild(newHeader)
+// newHeader.setAttribute(`id`, `victory`)
+// newheader.id = "Bryce is the champion"
+
+
+newHeader.id = "victory";
+newHeader.innerHTML = 'Bryce is the champion';
+
+//const header = document.getElementById('header');
+//header.innerHTML = 
